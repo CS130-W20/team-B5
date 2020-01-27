a530e72de43f45d09c9ec9eb767bd90498efea4f
Jiayu Hu <hujiayu96@gmail.com>
2020-01-26 20:14:05 -0800

    change theme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import { blue } from '@material-ui/core/colors'
+const theme = createMuiTheme({  palette: {    primary: blue  }})
+
+ReactDOM.render(<MuiThemeProvider theme={theme}> <App /> </MuiThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
