53d9cf82bc5e09cfb6e229320da5f63580e6b4ee
Yash Choudhary <yashc@g.ucla.edu>
2020-02-20 23:09:16 -0800

    Data and Model functionality test cases written

--- backend/tests/User.spec.js
+++ backend/tests/User.spec.js
@@ -16,6 +16,7 @@ const mockRequest = (sessionData) => {
 };
 
 describe("Operations performed on or by Users", () => {
+  
   test("Program should be able to create a user (req, res)", () => {
     const req = mockRequest({ username: 'test@gmail.com' }, {password: 'password'});
     const res = mockResponse();
@@ -23,6 +24,24 @@ describe("Operations performed on or by Users", () => {
     createUser(req, res);
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith("status_code:200");
+  });
+
+  test("Program won't create user if there is bad input (req, res)", () => {
+    const req = mockRequest({ username: 1234 }, {password: 'password'});
+    const res = mockResponse();
+
+    createUser(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith("status_code:400");
+  });
+
+  test("Program won't create user if user already exists (req, res)", () => {
+    const req = mockRequest({ username: 'test@gmail.com' }, {password: 'password'});
+    const res = mockResponse();
+
+    createUser(req, res);
+    expect(res.status).toHaveBeenCalledWith(409);
+    expect(res.json).toHaveBeenCalledWith("status_code:409");
   });
 
   test("Program should be able to log a user in (req, res)", () => {
@@ -33,7 +52,15 @@ describe("Operations performed on or by Users", () => {
     expect(res.json).toHaveBeenCalledWith("status_code:200");
   });
 
-  test("Program should be able to create a user (req)", () => {
+  test("Program won't log user in if bad credentials are inputted (req, res)", () => {
+    const req = mockRequest({ username: 'test@gmail.com' }, {password: '00'});
+    const res = mockResponse();
+    login(req, res);
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith("status_code:401");
+  });
+
+  test("Program should be able to log out a user (req)", () => {
     const req = mockRequest({ username: 'test@gmail.com' }, {password: 'password'});
     const res = mockResponse();
     logOut(req, res);
