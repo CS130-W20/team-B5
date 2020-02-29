d9a29c66c5e2017c2eed1323cf5f17b298b00c02
shsssc <songsichen123@gmail.com>
2020-02-29 14:40:01 -0800

    added delete gcloud storage support

--- backend/service/ModelService.js
+++ backend/service/ModelService.js
@@ -2,6 +2,7 @@
 const con = require('../utils/dbconn.js');
 const User = require('../service/UserService');
 const utils = require('../utils/writer.js');
+const gStorage = require('../utils/gcloud');
 
 /**
  * remove model the user owns
@@ -12,15 +13,25 @@ const utils = require('../utils/writer.js');
  * no response value expected for this operation
  **/
 exports.deleteModel = async function (model_id, session_token) {
-    //todo make sure file is deleted with google cloud API
     const uid = await User.token2uid(session_token);
     if (uid === -1) {
         return utils.respondWithCode(401, {
             "error": 'unauthorized'
         });
     }
-    const [d, _] = await con.promise().query('delete from models where owner = ? and id = ?', [uid, model_id]);
-    if (d.affectedRows === 1) {
+
+    const [d] = await con.promise().query('select * from models where owner = ? and id = ?', [uid, model_id]);
+    if (d.length === 1) {
+        try {
+            await gStorage.deleteFile(d[0].location);
+        } catch (e) {
+            console.log(e);
+        }
+
+    }
+
+    const [d1] = await con.promise().query('delete from models where owner = ? and id = ?', [uid, model_id]);
+    if (d1.affectedRows === 1) {
         return {};
     }
     return utils.respondWithCode(400, {
@@ -74,7 +85,7 @@ exports.modifyModel = async function (model_id, session_token, name, shared) {
         }
     }
 
-    if (shared!==undefined) {
+    if (shared !== undefined) {
         const [d, _] = await con.promise().query('update models set shared = ? where owner = ? and id = ?', [shared, uid, model_id]);
         if (d.affectedRows !== 1) {
             return utils.respondWithCode(400, {
