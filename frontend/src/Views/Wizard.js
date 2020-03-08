1dfaf798315b257a53c592d5faf47c2eb9369bf4
Jiayu Hu <hujiayu96@gmail.com>
2020-03-08 15:56:10 -0700

    Wizard Completed

--- frontend/src/Views/Wizard.js
+++ frontend/src/Views/Wizard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
@@ -7,184 +7,142 @@ import StepContent from '@material-ui/core/StepContent';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import {Grid, Box} from '@material-ui/core';
-
+import {Link} from "react-router-dom";
+import RLink from "@material-ui/core/Link";
 
 const useStyles = makeStyles(theme => ({
-    button: {
-        marginTop: theme.spacing(1),
-        marginRight: theme.spacing(1),
-    },
-    actionsContainer: {
-        marginBottom: theme.spacing(2),
-    },
-    resetContainer: {
-        padding: theme.spacing(3),
-    },
-    dragAndDrop: {
-        border: 'dotted',
-        height: 200,
-        maxWidth: 500,
-        margin: 'auto',
-        borderRadius: 25
-    }
+  button: {
+    marginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing(2),
+  },
+  resetContainer: {
+    padding: theme.spacing(3),
+  },
+  dragAndDrop: {
+    border: 'dotted',
+    height: 200,
+    maxWidth: 500,
+    margin: 'auto',
+    borderRadius: 25
+  },
+  noLinkDefault: {
+    textDecoration: 'none'
+  }
 }));
 
-function getSteps() {
-    return ['Upload Training Data', 'Create an ad group', 'Create an ad'];
-}
-
-function getStepContent(step) {
-    switch (step) {
-        case 0:
-            return `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`;
-        case 1:
-            return 'An ad group contains one or more ads which target a shared set of keywords.';
-        case 2:
-            return `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`;
-        default:
-            return 'Unknown step';
-    }
+function CustomLink(props) {
+  const classes = useStyles();
+  return <Link to={props.to}>
+    <RLink variant={props.variant}>
+      {props.value}
+    </RLink>
+  </Link>
 }
 
 export default function VerticalLinearStepper() {
-    const classes = useStyles();
-    const [activeStep, setActiveStep] = React.useState(0);
-    const steps = getSteps();
+  const classes = useStyles();
 
-    const handleNext = () => {
-        setActiveStep(prevActiveStep => prevActiveStep + 1);
-    };
+  const [temp, setTemp] = useState(0);
 
-    const handleBack = () => {
-        setActiveStep(prevActiveStep => prevActiveStep - 1);
-    };
+  const forceUpdate = () => {
+    setTemp((temp) => temp + 1);
+  }
 
-    const handleReset = () => {
-        setActiveStep(0);
-    };
+  const getActiveStep = () => {
+    let res = localStorage.getItem("step");
+    if (res == null)
+      return 0;
+    return parseInt(res);
+  };
 
-    return (
-        <div>
-            <Typography variant="h4">Wizard</Typography>
-            <Stepper activeStep={activeStep} orientation="vertical" align="center">
-                <Step key={1}>
-                    <StepLabel>Training Data Upload</StepLabel>
-                    <StepContent>
-                        <div>
-                            Instructions of how to prepare training data here
-                        </div>
-                        <div className={classes.dragAndDrop}>
-                            Drag and drop your zipped nifity files here.
-                        </div>
-                        <div>
-                            <Button
-                                disabled={activeStep === 0}
-                                onClick={handleBack}
-                                className={classes.button}
-                            >
-                                Back
-                            </Button>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                onClick={handleNext}
-                                className={classes.button}
-                            >
-                                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                            </Button>
-                        </div>
-                    </StepContent>
-                </Step>
-                <Step key={2}>
-                    <StepLabel>Model Training</StepLabel>
-                    <StepContent>
-                        <Typography>{getStepContent(0)}</Typography>
-                        <div className={classes.actionsContainer}>
-                            <div>
-                                <Button
-                                    disabled={activeStep === 0}
-                                    onClick={handleBack}
-                                    className={classes.button}
-                                >
-                                    Back
-                                </Button>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={handleNext}
-                                    className={classes.button}
-                                >
-                                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                                </Button>
-                            </div>
-                        </div>
-                    </StepContent>
-                </Step>
-                <Step key={3}>
-                    <StepLabel>Prediction Data Upload</StepLabel>
-                    <StepContent>
-                        <Typography>{getStepContent(0)}</Typography>
-                        <div className={classes.actionsContainer}>
-                            <div>
-                                <Button
-                                    disabled={activeStep === 0}
-                                    onClick={handleBack}
-                                    className={classes.button}
-                                >
-                                    Back
-                                </Button>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={handleNext}
-                                    className={classes.button}
-                                >
-                                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                                </Button>
-                            </div>
-                        </div>
-                    </StepContent>
-                </Step>
-                <Step key={4}>
-                    <StepLabel>Download segmented images</StepLabel>
-                    <StepContent>
-                        <Typography>{getStepContent(0)}</Typography>
-                        <div className={classes.actionsContainer}>
-                            <div>
-                                <Button
-                                    disabled={activeStep === 0}
-                                    onClick={handleBack}
-                                    className={classes.button}
-                                >
-                                    Back
-                                </Button>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={handleNext}
-                                    className={classes.button}
-                                >
-                                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                                </Button>
-                            </div>
-                        </div>
-                    </StepContent>
-                </Step>
-            </Stepper>
-            {activeStep === steps.length && (
-                <Paper square elevation={0} className={classes.resetContainer}>
-                    <Typography>All steps completed - you&apos;re finished</Typography>
-                    <Button onClick={handleReset} className={classes.button}>
-                        Reset
+  const setActiveStep = (stepID) => {
+    localStorage.setItem("step", stepID);
+    forceUpdate();
+  };
+
+  const handleNext = () => {
+    setActiveStep(getActiveStep() + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(getActiveStep() - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const stepHeader = [
+    [0, "Training Data Upload"],
+    [1, "Model Training"],
+    [2, "Prediction Data Upload"],
+    [3, "Download segmented images"]
+  ];
+
+  const stepContent = [
+    <Typography>
+      The first step is to upload a training data set.<br/>
+      <CustomLink to="/data" value="Go to the Data View"/>
+    </Typography>,
+    <Typography>
+      Now, it's time to create a model using the existent training data set.<br/>
+      <CustomLink to="/model" value="Go to the Model View"/>
+    </Typography>,
+    <Typography>
+      In order to make a prediction using the created model, you need to first upload a prediction data set in
+      the <CustomLink to="/data" value="Data View"/>, and then go to the <CustomLink to="/model" value="Model View"/> to
+      select a model and click run.
+    </Typography>,
+    <Typography>
+      You can check all the tasks in our system using the <CustomLink to="/task" value="Task View"/> view, and after the
+      prediction is done, we can go back to our <CustomLink to="/data" value="Data View"/> to find out the result and
+      download.
+    </Typography>
+  ];
+
+  return (
+    <div>
+      <Typography variant="h4">Wizard</Typography>
+      <Stepper activeStep={getActiveStep()} orientation="vertical" align="center">
+        {
+          stepHeader.map((step) => <Step key={step[0]}>
+              <StepLabel>{step[1]}</StepLabel>
+              <StepContent>
+                {stepContent[step[0]]}
+                <div className={classes.actionsContainer}>
+                  <div>
+                    <Button
+                      disabled={getActiveStep() === 0}
+                      onClick={handleBack}
+                      className={classes.button}
+                    >
+                      Back
                     </Button>
-                </Paper>
-            )}
-        </div>
-    );
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleNext}
+                      className={classes.button}
+                    >
+                      {step[0] >= stepHeader.length - 1 ? 'Finish' : 'Next'}
+                    </Button>
+                  </div>
+                </div>
+              </StepContent>
+            </Step>
+          )
+        }
+      </Stepper>
+      {getActiveStep() === stepHeader.length && (
+        <Paper square elevation={0} className={classes.resetContainer}>
+          <Typography>
+            All steps completed, you&apos;re finished. <RLink onClick={handleReset}>Click here to restart!</RLink>
+          </Typography>
+        </Paper>
+      )}
+    </div>
+  );
 }
